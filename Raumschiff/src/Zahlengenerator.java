0e1b308de0508b2a6d8e0684fe1dc62b4deb924a
Lukas Schulz <murtagh.1111@gmx.de>
2016-12-27 20:05:59 +0100

    Adding the Raumschiff-Project

---
+++ Raumschiff/src/Zahlengenerator.java
@@ -0,0 +1,12 @@
+
+public class Zahlengenerator {
+	public static void main(String[]args){
+		int zahl = 879;
+		while(zahl!=1122){
+			zahl=zahl+1;
+			System.out.print(zahl+", ");
+			
+			
+		}
+	}
+}
